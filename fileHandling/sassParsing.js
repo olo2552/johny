ec4c4d905a4579e23fe5cdad2be1e30f8d290583
olo2552 <a.wielgorski@protonmail.com>
2018-09-14 15:58:21 +0200

    #5 first iteration of sass file parsing

---
+++ fileHandling/sassParsing.js
@@ -0,0 +1,17 @@
+const buildColorMapFromSASS = sassStreamFilePath => (
+    sassStreamFilePath.split('\n')
+        .trim()
+        .filter(line => line.includes('#'))
+        .map(line => line.split(':'))
+        .map(line => line.map(line => line.trim()))
+        .map(line => [line[0].slice(1), line[1].slice(0, -1)])
+        .map(line => [line[0], line[1].toUpperCase()])
+        .map(line => line.reverse())
+        .reduce((colorsObj, line) => Object.assign(colorsObj, {
+            [line[0]]: line[1],
+        }), {})
+);
+
+
+
+export { buildColorMapFromSASS };
