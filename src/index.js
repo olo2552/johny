1ed59401ba82be11debe37e29a069ce2e2191db4
olo2552 <a.wielgorski@protonmail.com>
2018-09-15 12:19:18 +0200

    #7 moving index to /src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const { readSassFile } = require('./src/fileHandling/readSassFile');
-const { buildColorMapFromSass } = require('./src/fileHandling/buildColorMapFromSass');
-const { findNearestColor } = require('./src/colorUtils/findNearestColor');
+const { readSassFile } = require('./fileHandling/readSassFile');
+const { buildColorMapFromSass } = require('./fileHandling/buildColorMapFromSass');
+const { findNearestColor } = require('./colorUtils/findNearestColor');
 
 const colorToMatch = process.argv[2] || '#1ED760';
 const sassVariablesFilePath = process.argv[3] || '/home/olo2552/Desktop/_variables.scss';
