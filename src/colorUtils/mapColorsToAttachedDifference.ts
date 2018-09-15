f84b3b3fb7964364b2710734bbdef21137e4760f
olo2552 <a.wielgorski@protonmail.com>
2018-09-15 17:44:03 +0200

    #7 fixing regression

--- src/colorUtils/mapColorsToAttachedDifference.ts
+++ src/colorUtils/mapColorsToAttachedDifference.ts
@@ -4,7 +4,7 @@ import { ColorWithDifference } from "../interfaces";
 function mapColorsToAttachedDifference (queryColor: string, colors: Array<string>): Array<ColorWithDifference> {
     return colors.map(color => ({
         color,
-        difference: differentiateRgb(queryColor)(color),
+        difference: differentiateRgb(queryColor, color),
     }));
 }
 
