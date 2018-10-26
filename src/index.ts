b48e6459fc119f755013988e75e90b4300f5f655
olo2552 <a.wielgorski@protonmail.com>
2018-10-26 23:25:05 +0200

    #8 declarationPairs index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import { findNearestColor } from "./colorUtils/findNearestColor";
-import { buildColorMapFromSass } from "./fileHandling/buildColorMapFromSass";
+import gonzalesPe from "gonzales-pe";
+import { inspect } from "util";
+import { filterVariablesDeclarations } from "./fileHandling/filterVariablesDeclarations";
+import { mapDeclarationsToVariablePairs } from "./fileHandling/mapDeclarationsToVariablePairs";
 import { readSassFile } from "./fileHandling/readSassFile";
 
-const colorToMatch: string = process.argv[2] || "#1ED760";
-const sassVariablesFilePath: string =
+const sassVariablesFilePath =
   process.argv[3] || "/home/olo2552/Desktop/_variables.scss";
 
 readSassFile(sassVariablesFilePath)
-  .then(file => buildColorMapFromSass(file))
-  .then(Object.keys)
-  .then(colors => findNearestColor(colorToMatch, colors))
-  // tslint:disable-next-line:no-console
-  .then(console.log);
+  .then(sass =>
+    gonzalesPe.parse(sass, {
+      syntax: "sass",
+      tabSize: 2
+    })
+  )
+  .then(filterVariablesDeclarations)
+  .then(parseTrees => parseTrees.map(parseTree => parseTree.toJson()))
+  .then(parseTrees => parseTrees.map(parseTree => JSON.parse(parseTree)))
+  .then(parseTrees => parseTrees[0])
+  .then(mapDeclarationsToVariablePairs)
+  .then(x =>
+    console.log(
+      inspect(x, {
+        colors: true,
+        compact: false,
+        depth: 30,
+        maxArrayLength: 100
+      })
+    )
+  );
